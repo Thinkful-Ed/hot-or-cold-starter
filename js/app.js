209c990e47738728b5a7e870f496e9ac69125bf2
Thinkful FEWD <fewd@thinkful.com>
2014-02-04 10:35:06 -0500

    starting point for hot or cold project

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+	
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+});
+
+
