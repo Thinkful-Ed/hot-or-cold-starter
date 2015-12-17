d72cbaf498d064afae81d63ab0a56f441759809b
Mike Hohne <mikehohne@gmail.com>
2015-12-17 11:20:24 -0700

    added comments

--- js/app.js
+++ js/app.js
@@ -12,6 +12,21 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
 });
 
+//var newGame = function() 
+//math.random to generate new random numbers
+//clear out form
+//form: submit test number against random number hot,cold or correct.
+//.click run another function
+  //get the value they've entered
+  //test against random #
 
+/* $("newGamebutton").click(function() {
+      reset form
+      reset guesses #
+      reset array
+      reset random #
+}
