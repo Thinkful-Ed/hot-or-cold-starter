4dbb6f19ef5ec76e35e0c34a04aa166ecee4707d
Aric Johnson <aric87@comcast.net>
2015-12-28 20:25:27 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ $(document).ready(pageLoad);
   	//fetch dom objects
   	newButton = $('a.new');
   	form = $('form');
-  	input = $form.find('#userGuess');
+  	input = form.find('#userGuess');
   	feedback = $('#feedback');
   	count = $('#count');
   	guessList = $('#guessList');
