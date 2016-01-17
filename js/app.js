5be739b2301d043d23c1292cb5bd79428237dd87
Chris Johnson <csjohnson79@icloud.com>
2016-01-17 14:29:33 -0700

    CHANGES ON 1/17 DURING SKYPE WITH MARIUS

--- js/app.js
+++ js/app.js
@@ -1,18 +1,7 @@
 
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
 
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
-});
 
 
 // This function generates the random number
@@ -26,24 +15,28 @@ function secretNum(min, max) {
 var secretNumber = secretNum(1, 100);
 console.log("Secret Number: " + secretNumber);
 
-
+/*backgroundColor change included in JS document so that it may change colors,
+depending on whether the guess was hotter or colder..*/
 document.body.style.backgroundColor = '#333';
 
-
+//the player's old guess must start at 0.
 var oldGuess = 0;
 
-
+/* this is a jQuery function that tags the 'count' ID in the CSS file, applies
+ the .text method to it to insert text into the HTML based on what was put
+ into the variable 'counter'.*/
 var counter = 30;
 $('#count').text(counter);
 
-
+// the location.reload function is used to reload the page.
 function newGame() {
     document.location.reload(true);
 }
 
 
  
-
+/*This section sets the boundaries for how hot/cold the user is, and the
+parameters that each temperature definition lies in */
 function guessFeedback (secretNumber, guessedNumber) {
     var difference = Math.abs(secretNumber - guessedNumber);
     if (difference >= 50) {
@@ -70,7 +63,114 @@ function guessFeedback (secretNumber, guessedNumber) {
 }
 
 
+    // Function to provide "relative feedback" to the user (based on what the user's prior guess was).
+    function relativeFeedback(secretNumber, oldGuess, newGuess) {
+        var oldDiff = parseInt(Math.abs(secretNumber - oldGuess));
+        var newDiff = parseInt(Math.abs(secretNumber - newGuess));
+        if (newDiff > oldDiff) {
+            $('#relative-feedback').text('You are colder than the last guess!');
+        } else if (newDiff === oldDiff) {
+            $('#relative-feedback').text('You are as far as your previous guess!');
+        } else {
+            $('#relative-feedback').text('You are hotter than the last guess!');
+        }
+    }
+
+    // Function to count the number of guesses
+    function guessCounter(counter) {
+        $('#count').text(counter);
+    }
+
+    // Function to show the history of guesses
+    function guessHistory() {
+        $('#guessList').append('<li>' + parseInt($('#userGuess').val(), 10) + '</li>');
+    }
 
 
+    // Function to implement a simple validation of the user input
+    function validation(guessedNumber) {
 
+        //check the guessed number in the console
+        console.log("Guessed Number: " + guessedNumber)
+
+        //if the number is divisible by 1 it means it is an integer (it has no decimals)
+        if (guessedNumber % 1 !== 0) {
+            alert('You must enter an integer value!!');
+            //reset the guess value to nothing
+            $('#userGuess').val('');
+            return false; // this means, stop the loop and don't do anything else
+        }
+
+        //if the guessedNumber is smaller than 1 OR the guessedNumber is bigger than 100...
+        else if (guessedNumber < 1 || guessedNumber > 100) {
+            alert('Please guess a number between 1 to 100!!');
+            //reset the guess value to nothing
+            $('#userGuess').val('');
+            return false; // this means, stop the loop and don't do anything else
+        }
+
+        //else the guessedNumber is valid
+        else {
+            guessFeedback(secretNumber, guessedNumber);
+            counter--;
+            guessHistory();
+            $('#userGuess').val('');
+        }
+
+        //if the number of guesses is smaller than 0 it means that the game is over
+        if (counter <= 0) {
+            $('#feedback').text('Game Over!');
+            document.getElementById("userGuess").disabled = true;
+            document.getElementById("guessButton").disabled = true;
+            alert('The Secret number was ' + secretNumber + ' ! Better luck next time !!');
+        }
+
+        //update the number of guesses
+        guessCounter(counter);
+    }
+
+// declared functions above, invoked functions below.....
+
+    $('.new').on('click', newGame);
+
+    $('#guessButton').on('click', function () {
+        var guessedNumber = parseInt($('#userGuess').val(), 10);
+        var newGuess = parseInt(guessedNumber);
+
+        validation(guessedNumber);
+
+        if (oldGuess !== 0 && guessedNumber >= 1 && guessedNumber <= 100) {
+            relativeFeedback(secretNumber, oldGuess, newGuess);
+        }
+        oldGuess = newGuess;
+    });
+
+    $('#userGuess').on('keypress', function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            var guessedNumber = parseInt($('#userGuess').val(), 10);
+            var newGuess = parseInt(guessedNumber);
+
+            validation(guessedNumber);
+
+            if (oldGuess !== 0 && guessedNumber >= 1 && guessedNumber <= 100) {
+                relativeFeedback(secretNumber, oldGuess, newGuess);
+            }
+            oldGuess = newGuess;
+        }
+    });
+
+
+    /*--- Display information modal box ---*/
+    $(".what").click(function(){
+        $(".overlay").fadeIn(1000);
+
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+        $(".overlay").fadeOut(1000);
+    });
+
+});
 
