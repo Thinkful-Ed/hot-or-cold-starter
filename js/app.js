f6930bcc4d8911876d968a5ad9883fb8c215d280
git config --global user.name "Link1965 <git config --global user.email nicci.hydak@gmail.com>
2016-02-22 19:28:57 -0500

    Addition of differences in #'s

--- js/app.js
+++ js/app.js
@@ -5,27 +5,50 @@ $(document).ready(function(){
   var allGuesses = [];
   var diff;
   var message;
+  var guess;
 
     $("#guessButton").click(function() {
-      var guess = $("#userGuess").val();
-      $("#userGuess").val('');
-      allGuesses.push(guess);
-      $("#guessList").append("<li>" + guess + "</li>")
-      console.log('Guess = ' + guess);
+    	var guess = $("#userGuess").val();
+  		$("#userGuess").val('');
+    	  allGuesses.push(guess);
+     	 if (guess != '') {
+     		 $("#guessList").append("<li>" + guess + "</li>");
+     }
+      	console.log('Guess = ' + guess);
+    
 
       diff = Math.abs(guess - randomNumber);
 
-      if (diff > 80) {
-        message = "You're ice cold!"
-      } else if (diff > 50) {
-        message = "Pretty cold!"
-      } else if (guess == randomNumber) {
+      if (diff >= 80) {
+         message = "You're ice cold!"
+      } 
+
+      else if ((diff >= 60) && (diff < 80)) {
+        message = "Very cold!"
+      }
+
+      else if ((diff >= 40) && (diff < 60)) {
+      	message = "Cold!"
+      }
+
+      else if ((diff >= 20) && (diff < 40)) {
+      	message = "Still pretty chilly!"
+      }
+
+      else if ((diff >= 11) && (diff < 20)) {
+    	 message = "You're warming up!"
+    }
+      else if ((diff >= 1) && (diff < 11)) {
+      	message = "You're hot!"
+      }
+
+	  else if (guess == randomNumber) {
         message = "You got it!"
       }
-      $("#feedback").text(message);
 
-      return false;
-    });
+	$("#feedback").text(message);
+    return false;
+    
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -38,6 +61,10 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  });
+
 });
+
+
 
 
