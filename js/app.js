307d5cbe2e4469c75fb6e7260abeb0b940e80072
Tess <tessamero@gmail.com>
2016-06-12 18:02:10 -0700

    code hints

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 
-$(document).ready(function(){
+/* So I set it up to give you a hint on how to set up your code... hope this helps for you! */
+
+$(document).ready(function(){  /* everything will be done within these brackets. */
+/* Create your variables here */
+
+/* Create New Game, returning a function and that's it */
+newGame();
+/* What happens when you submit the form? This */
+
+/* Function Create New Game, with code inside brackets */
+function newGame() {
+	// do this
+}
+/*--- Display information modal box ---*/
+  $(".what").click(function(){
+  	$(".overlay").fadeIn(1000);
+});
+/*--- Hide information modal box ---*/
+ $("a.close").click(function(){
+ 	$(".overlay").fadeOut(1000);
+});
+/* Create a Random Number Function */
+
+/* JavaScript validation - make sure the input has rules. ie: only an integer between 1 and 100... */
+
+/* Determine if it's Hot or Cold Function */
+
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
 });
 
 
