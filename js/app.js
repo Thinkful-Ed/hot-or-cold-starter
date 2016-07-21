62774e490c7ea5207fdd66bf7ae1c7da422e61c1
Cassandra <cmcorrales3@gmail.com>
2016-07-21 18:14:45 -0400

    generate random number when user clicks 'new game'

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -14,4 +14,22 @@ $(document).ready(function(){
 
 });
 
+/*Create a function that generates a random number to start a new game */
+function newGame() {
+/* generate a random number from 1-100 */
+		var randomNumber = Math.floor((Math.random()*100)+1);
+		console.log(randomNumber);
+/*When user clicks on "new game", console.log a random number*/
+	}
 
+	var newClass = document.getElementById('newButton');
+	newClass.addEventListener('click', newGame);
+/* Add event listener to "new game" button*/
+/* Take user guess, and determine if it's close to randomNumber */
+function checkGuess() {
+	var guess = document.getElementById('userGuess');
+	var guessDiff = randomNumber - guess;
+	if (guessDiff < abs(10)) {
+		return
+	}
+}
