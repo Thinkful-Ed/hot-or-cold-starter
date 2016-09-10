39dcaa35725349352cdfa850af55df1248d27f41
Deborah Kain <deborahkain@mac.com>
2016-09-10 12:22:27 -0700

    first pair commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 	
+  var randomNumber = secretNumber();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +14,55 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
+    $(".new").click(function(){
+      newGame();
+    });
+    function clear() { //clear html and texbox// 
+      $('.clear').on('click', function() { 
+        $('.result').empty(); 
+        $('#userInput').val(''); 
+      }); 
+    }
+    $("#count").html(parseInt($("#count").html())+1);
+    
+    function newGame(){
+      // numbers will be placed in (remove those numbers)
+      // tells you whether you are near or far from the answer
+      randomNumber = secretNumber();
+      console.log("Inside newGame", randomNumber);
+      // return x;
+    }
+    button.addEventListener("click", function userGuess(){
+
+    }) 
+    function userGuess(){
+      if (userGuess %1 !== 0) { 
+        alert ("Please submit a whole number");
+
+      }
+      else {
+
+      }
+        else if (userGuess <= secretNumber -50 || userGuess <= secretNumber + 50) {
+          alert ("Scorching Hot");
+        }
+    }
+
+    function secretNumber(){
+      return Math.floor((Math.random() * 100) + 1);
+    }
 });
+
+//function clear() { //clear html and texbox $('.clear').on('click', function() { $('.result').empty(); $('#userInput').val(''); }); }
+//else if (userGuess <= secretNumber -50) {alert ("Sorching Hot")}
+//$("#feedback").append("Icy Cold")//reports results into feedback
+
+//(value % 1 !== 0) //makes sure guess isn't a decimal
+//if (userGuess %1 !== 0) { alert ("Please submit a whole number")}
+
+
+
 
 
